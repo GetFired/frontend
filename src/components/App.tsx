f783e488764dce105109bcc686bfdd20483ede11
David Haas <davidhaas6@gmail.com>
2021-03-27 12:43:00 -0500

    init

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import logo from '../logo.svg';
+import './App.css';
+
+function App() : JSX.Element {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="Title">GetFired</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Chart.tsx
@@ -0,0 +1,1 @@
+import ApexCharts from 'apexcharts'
